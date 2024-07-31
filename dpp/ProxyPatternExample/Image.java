69419677abf2b38287547341c030cf5915bf5061
saita123 <103350318+saita123@users.noreply.github.com>
2024-07-31 15:48:11 +0530

    Delete dpp/ProxyPatternExample directory

--- dpp/ProxyPatternExample/Image.java
+++
@@ -1,5 +0,0 @@
-package com.example.proxy;
-
-public interface Image {
-    void display();
-}
--- dpp/ProxyPatternExample/PaymentStrategy.java
+++
@@ -1,5 +0,0 @@
-package com.example.strategy;
-
-public interface PaymentStrategy {
-    void pay(double amount);
-}
